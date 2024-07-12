839567daf96ac84267a273fa41ab0b8d7fa0c425
LC1CUchibori <146916712+LC1CUchibori@users.noreply.github.com>
2024-07-12 15:43:46 +0900

    02-09_確認課題

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -10,10 +10,27 @@ void Enemy::Initialize(Model* model, ViewProjection* viewProjection, const Vecto
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
 	worldTransform_.rotation_.y = -3.14f / 2.0f;
+
+	// 速度を設定する
+	velocity_ = { -kWalkSpeed,0,0 };
+
+	// 時間を設定する
+	walkTimer_ = 0.0f;
 }
 
 void Enemy::Updata()
 {
+	// 移動
+	worldTransform_.translation_ += velocity_;
+
+	// タイマーを加算
+	walkTimer_ += 1.0f / 60.0f;
+
+	// 回転アニメーション
+	float param = std::sin(walkTimer_ * 3.14f);
+	float radian = (kWalkMotionAngleStart + kWalkMotionAngleEnd) * (param + 1.0f) / 2.0f;
+	worldTransform_.rotation_.x = radian;
+
 	// 行列計算
 	worldTransform_.UpdateMatrix();
 }
--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -26,6 +26,20 @@ private:
 	Model* model_ = nullptr;
 	// ビュープロジェクションのポインタ
 	ViewProjection* viewProjection_ = nullptr;
-
+	// マップチップフィールドのポインタ
 	MapChipField* mapChipField_ = nullptr;
+
+	// 歩行の速さ
+	static inline const float kWalkSpeed = 0.01f;
+	// 速度
+	Vector3 velocity_ = {};
+
+	// 最初の角度[度]
+	static inline const float kWalkMotionAngleStart = -7.0f;
+	// 最後の角度[度]
+	static inline const float kWalkMotionAngleEnd = 8.0f;
+	// アニメーションの周期となる時間[秒]
+	static inline const float kWalkMotionTime = 5.0f;
+	// 経過時間
+	float walkTimer_ = 0.0f;
 };
